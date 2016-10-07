6e3004a022ebf6edb862487fa7ca085c6ef33b80
jbudge3 <jake.budge@me.com>
2016-10-06 20:43:07 -0600

    Modified profile view route

--- config.js
+++ config.js
@@ -12,14 +12,26 @@ angular.module('devMtnSocial')
           templateUrl: '/components/entry/entry.html',
           controller: 'entryCtrl'
         },
-        footer: {}
+        footer: {
+          templateUrl: 'components/footer/footer.html'
+        }
       }
     })
 
     .state('profile', {
       url: '/profile',
-      templateUrl: '/components/profile/profile.html',
-      controller: 'profileCtrl'
+      views: {
+        header: {
+          templateUrl: '/components/headers/header1.html'
+        },
+        body: {
+          templateUrl: '/components/profile/profile.html',
+          controller: 'profileCtrl'
+        },
+        footer: {
+          templateUrl: 'components/footer/footer.html'
+        }
+      }
     })
 
   $urlRouterProvider
